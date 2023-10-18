9a2695ede8bce500705e9fd40fd733f87e7aa405
Carlos Osiris Lopez <ozzlopz@gmail.com>
2023-10-18 09:01:41 -0600

    Merging changes

--- functions/services/ApiService.js
+++ functions/services/ApiService.js
@@ -4,7 +4,8 @@ const cors = require('cors')({ origin: true })
 const { https, logger } = functions
 
 const { uploadPdf } = require('./PinataService')
-const { /*getHypercertsCounts,*/ searchHypercerts } = require('./HypercertsService')
+const { getHypercerts, searchHypercerts } = require('./HypercertsService')
+const { searchReviewsByHypercertID } = require('./ReviewsService')
 
 const app = express()
 
@@ -33,25 +34,42 @@ app.post('/generate_pdf', (request, response) => {
     }
   })
 })
-/*
-app.get('/get_hypercerts_counts', (request, response) => {
+
+app.get('/get_hypercerts', (request, response) => {
   return cors(request, response, async () => {
     try {
-      const hypercertsCounts = await getHypercertsCounts()
-      response.send(JSON.stringify(hypercertsCounts))
+      const hypercerts = await getHypercerts()
+      response.send(hypercerts)
     } catch (error) {
       logger.error('[ !!! ] Error: ', error)
       throw new https.HttpsError(error.code, error.message)
     }
   })
 })
-*/
+
 app.get('/search_hypercerts', (request, response) => {
   return cors(request, response, async () => {
     try {
-      const { searchInput } = request.query
-      const hypercertResults = await searchHypercerts(searchInput)
+      const { searchInput, lastSixMonths } = request.query
+      const hypercertResults = await searchHypercerts(
+        searchInput,
+        lastSixMonths,
+      )
       response.send(hypercertResults)
+    } catch (error) {
+      logger.error('[ !!! ] Error: ', error)
+      throw new https.HttpsError(error.code, error.message)
+    }
+  })
+})
+
+app.get('/search_reviews', (request, response) => {
+  return cors(request, response, async () => {
+    try {
+      const { hypercertID } = request.query
+      const reviewsResults = await searchReviewsByHypercertID(hypercertID)
+
+      response.send(reviewsResults)
     } catch (error) {
       logger.error('[ !!! ] Error: ', error)
       throw new https.HttpsError(error.code, error.message)
